ef85081caa435c4478a17e8ad0c0746d3ac98efa
Mohsen Banan <form@mohsen.banan.1.byname.net>
2021-11-03 15:24:42 -0700

    setup.py 0.10 cntnr bpo pals etc.

--- py3/setup.py
+++ py3/setup.py
@@ -17,7 +17,7 @@ def longDescription():
 
 
 #__version__ = get_version('unisos/icm/__init__.py')
-__version__ = '0.9'
+__version__ = '0.10'
 
 
 requires = [
@@ -27,6 +27,12 @@ requires = [
     'bisos.things',
     'bisos.gossonot',
     'bisos.icm',
+    'bisos.basics',
+    'bisos.bpo',
+    'bisos.cntnr',
+    'bisos.coreDist',
+    'bisos.currents',
+    'bisos.pals',
 ]
 # 'uniso.x822Msg',
 # 'unisos.marme',
